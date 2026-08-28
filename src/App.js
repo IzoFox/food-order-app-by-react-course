a5f0d477a135198f9a4146ef621b054b25a4e70f
Viktor Liashenko <izofox@gmail.com>
2026-08-28 08:48:03 +0200

    Lesson 115: Added a Header component, styles and a sushi image for the header component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/Layout/Header';
 
 function App() {
   return (
-    <div>
-      <h2>Start project</h2>
-    </div>
+    <React.Fragment>
+      <Header />
+    </React.Fragment>
   );
 }
 
