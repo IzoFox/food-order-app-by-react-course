10207d615761d3431c072891325a1e1c21283b37
Viktor Liashenko <izofox@gmail.com>
2026-09-03 13:48:47 +0200

    Lesson 122: Added a Modal component.
    This component is a wrapper to display information in a modal window.

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,3 +1,4 @@
+import Modal from '../UI/Modal';
 import styles from './Cart.module.css';
 
 const Cart = (props) => {
@@ -16,7 +17,7 @@ const Cart = (props) => {
     );
 
     return (
-        <div>
+        <Modal>
             {cartItems}
             <div className={styles.total}>
                 <span>Total</span>
@@ -26,7 +27,7 @@ const Cart = (props) => {
                 <button className={styles['button--alt']}>Close</button>
                 <button className={styles.button}>Order</button>
             </div>
-        </div>
+        </Modal>
     )
 }
 
