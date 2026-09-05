34f8febb2e5b3f27cb4c9e532cf39ac7a250697b
Viktor Liashenko <izofox@gmail.com>
2026-09-05 08:43:46 +0200

    Lesson 127: using refs.
    - Implemented function to count elements in the cart.

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,31 @@
+import { useRef, useState } from 'react';
 import Input from '../../UI/Input';
 import styles from './MealItemForm.module.css';
 
 const MealItemForm = (props) => {
+
+    const [isAmountValid, setIsAmountValid] = useState(true);
+
+    const amountInputRef = useRef();
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const inputAmount = amountInputRef.current.value;
+        if(inputAmount.trim().length === 0 || +inputAmount < 1 || +inputAmount > 10) {
+            setIsAmountValid(false);
+            return;
+        }
+
+        props.onAddToCart(+inputAmount);
+    }
+
     return(
-        <form className={styles.form}>
-            <Input label='Qty' input={{
+        <form className={styles.form} onSubmit={submitHandler}>
+            <Input 
+            ref={amountInputRef}
+            label='Qty' 
+            input={{
                 id: props.id,
                 type: 'number',
                 min: '1',
@@ -12,6 +33,7 @@ const MealItemForm = (props) => {
                 defaultValue: '1'
             }} />
             <button>Add</button>
+            {!isAmountValid && <p>Please, fill in quantity from 1 to 10.</p>}
         </form>
     )
 }
