be3aa4d9b6e3607677538eb33a1ef1646b0035b6
Viktor Liashenko <izofox@gmail.com>
2026-09-05 12:48:25 +0200

    Lesson 130: adding functionality to remove elements from the cart.
    - Added a function to remove elements from the cart.
    - Fixed a key name in removeItemHandler. Changed "item: id" to "id: id".

--- src/store/CartContextProvider.js
+++ src/store/CartContextProvider.js
@@ -40,6 +40,33 @@ const cartReducer = (state, action) => {
             totalAmount: updatedTotalAmount
         }
     }
+    if (action.type === 'REMOVE_ITEM') {
+        const existingCartItemIndex = state.items.findIndex((item) => {
+            return item.id === action.id;
+        });
+
+        const existingCartItem = state.items[existingCartItemIndex];
+
+        const updatedTotalAmount = state.totalAmount - existingCartItem.price;
+
+        let updatedItems;
+        if (existingCartItem.amount === 1) {
+            updatedItems = state.items.filter((item) => item.id !== action.id)
+        } else {
+            const updatedItem = {...existingCartItem, amount: existingCartItem.amount - 1}
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem;
+        }
+
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount,
+        }
+
+
+
+
+    }
     return defaultCartState;
 }
 
@@ -56,7 +83,7 @@ const CartContextProvider = (props) => {
     const removeItemHandler = id => {
         dispatchCartAction({
             type: 'REMOVE_ITEM',
-            item: id,
+            id: id,
         });
     };
 
